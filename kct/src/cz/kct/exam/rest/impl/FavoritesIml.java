1b4302f76b168adbc9b29e048dfb9fe3e4d7ba15
Vojtech_Wrnata <vojtech.wrnata@red-fox.cz>
2021-01-16 14:58:05 +0100

    implementace API a napojeni na DB

--- kct/src/cz/kct/exam/rest/impl/FavoritesIml.java
+++ kct/src/cz/kct/exam/rest/impl/FavoritesIml.java
@@ -1,10 +1,17 @@
 package cz.kct.exam.rest.impl;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.SecurityContext;
 
-import io.swagger.model.FavoriteEntity;
+import cz.kct.exam.db.favorites.FavoriteEntity;
+import cz.kct.exam.db.favorites.FavoritesFacade;
+import io.swagger.model.FavoriteRestEntity;
+import io.swagger.model.ApiResponse;
 
 /**
  * Implementace REST API /v1/favorites/
@@ -14,39 +21,137 @@ import io.swagger.model.FavoriteEntity;
  */
 public class FavoritesIml {
 
+    /**
+     * Nacte z DB Oblibena plemena, prevede na REST a vrati jako REST Response
+     *  
+     * @param securityContext
+     * 
+     * @return Response REST Response
+     */
     public Response getFavorites(SecurityContext securityContext) {
         System.out.println("getFavorites");
-        FavoriteEntity favorites = new FavoriteEntity();
+        List<FavoriteRestEntity> restFavorites = new ArrayList<FavoriteRestEntity>();
         Status responseStatus = Status.OK;
-        
-        return Response.status(responseStatus).encoding("UTF-8").header("Content-type", "application/json")
-                .entity(favorites).build();
+        String errorMessage = "";
+
+        FavoritesFacade facade = new FavoritesFacade();
+        List<FavoriteEntity> favorites = null;
+        try {
+            favorites = facade.getFavorites();
+            for (FavoriteEntity favoriteEntity : favorites) {
+                restFavorites.add(new FavoriteRestEntity(favoriteEntity));
+            }
+            if (!restFavorites.isEmpty()) {
+                return Response.status(responseStatus).encoding("UTF-8").header("Content-type", "application/json")
+                        .entity(restFavorites).build();
+            } else {
+                responseStatus = Status.NO_CONTENT;
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getLocalizedMessage());
+            responseStatus = Status.INTERNAL_SERVER_ERROR;
+            errorMessage = e.getLocalizedMessage();
+        } catch (Exception e) {
+            System.err.println(e.getLocalizedMessage());
+            responseStatus = Status.INTERNAL_SERVER_ERROR;
+            errorMessage = e.getLocalizedMessage();
+        }
+
+        return Response.ok(new ApiResponse(responseStatus.name(), errorMessage)).status(responseStatus).build();
     }
-    
+
+    /**
+     * Nacte z DB Oblibene plemeno podle ID, prevede na REST a vrati jako REST Response
+     *  
+     * @param securityContext
+     * 
+     * @return Response REST Response
+     */
     public Response getFavoriteById(String favoriteId, SecurityContext securityContext) {
         System.out.println("getFavoriteById");
-        FavoriteEntity favorites = new FavoriteEntity();
+        FavoriteRestEntity restFavorite = null;
         Status responseStatus = Status.OK;
-        
-        return Response.status(responseStatus).encoding("UTF-8").header("Content-type", "application/json")
-                .entity(favorites).build();
+        String errorMessage = "";
+
+        FavoritesFacade facade = new FavoritesFacade();
+        FavoriteEntity favorite = null;
+        try {
+            favorite = facade.getFavorite(favoriteId);
+            restFavorite = new FavoriteRestEntity(favorite);
+            if (null != restFavorite && !restFavorite.getId().equals("")) {
+                return Response.status(responseStatus).encoding("UTF-8").header("Content-type", "application/json")
+                        .entity(restFavorite).build();
+            } else {
+                responseStatus = Status.NO_CONTENT;
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getLocalizedMessage());
+            responseStatus = Status.INTERNAL_SERVER_ERROR;
+            errorMessage = e.getLocalizedMessage();
+        } catch (Exception e) {
+            System.err.println(e.getLocalizedMessage());
+            responseStatus = Status.INTERNAL_SERVER_ERROR;
+            errorMessage = e.getLocalizedMessage();
+        }
+
+        return Response.ok(new ApiResponse(responseStatus.name(), errorMessage)).status(responseStatus).build();
     }
-    
-    public Response addFavorite(FavoriteEntity favorite, SecurityContext securityContext) {
-        System.out.println("addFavorite");
-        FavoriteEntity favorites = new FavoriteEntity();
+
+    /**
+     * Prida do DB Oblibene plemeno
+     *  
+     * @param securityContext
+     * 
+     * @return Response Status.OK, nebo Status.INTERNAL_SERVER_ERROR
+     */
+    public Response addFavorite(FavoriteRestEntity restFavorite, SecurityContext securityContext) {
+        System.out.println("addFavorite, " + restFavorite);
+        FavoriteEntity favorite = new FavoriteEntity(restFavorite);
         Status responseStatus = Status.OK;
+        String errorMessage = "";
         
-        return Response.status(responseStatus).encoding("UTF-8").header("Content-type", "application/json")
-                .entity(favorites).build();
+        FavoritesFacade facade = new FavoritesFacade();
+        try {
+            facade.addFavorite(favorite);
+            return Response.status(responseStatus).encoding("UTF-8").header("Content-type", "application/json")
+                    .entity(new FavoriteRestEntity(favorite)).build();
+        } catch (SQLException e) {
+            System.err.println(e.getLocalizedMessage());
+            responseStatus = Status.INTERNAL_SERVER_ERROR;
+            errorMessage = e.getLocalizedMessage();
+        } catch (Exception e) {
+            System.err.println(e.getLocalizedMessage());
+            responseStatus = Status.INTERNAL_SERVER_ERROR;
+            errorMessage = e.getLocalizedMessage();
+        }
+        return Response.ok(new ApiResponse(responseStatus.name(), errorMessage)).status(responseStatus).build();
     }
-    
-    public Response removeFavorite(String favoriteId, SecurityContext securityContext) {
-        System.out.println("removeFavorite");
-        FavoriteEntity favorites = new FavoriteEntity();
+
+    /**
+     * Smaze z DB Oblibene plemeno
+     *  
+     * @param securityContext
+     * 
+     * @return Response Status.OK, nebo Status.INTERNAL_SERVER_ERROR
+     */
+     public Response removeFavorite(String favoriteId, SecurityContext securityContext) {
+        System.out.println("removeFavorite, " + favoriteId);
         Status responseStatus = Status.OK;
+        String errorMessage = "";
         
-        return Response.status(responseStatus).encoding("UTF-8").header("Content-type", "application/json")
-                .entity(favorites).build();
+        FavoritesFacade facade = new FavoritesFacade();
+        try {
+            facade.deleteFavorite(favoriteId);
+            return Response.ok(new ApiResponse(responseStatus.name(), errorMessage)).status(responseStatus).build();
+        } catch (SQLException e) {
+            System.err.println(e.getLocalizedMessage());
+            responseStatus = Status.INTERNAL_SERVER_ERROR;
+            errorMessage = e.getLocalizedMessage();
+        } catch (Exception e) {
+            System.err.println(e.getLocalizedMessage());
+            responseStatus = Status.INTERNAL_SERVER_ERROR;
+            errorMessage = e.getLocalizedMessage();
+        }
+        return Response.ok(new ApiResponse(responseStatus.name(), errorMessage)).status(responseStatus).build();
     }
 }
